b6724c9a970b794547fc66bf33f9720cd0defaa6
kbarry <g00339811@gmit.ie>
2018-11-29 02:47:43 +0000

    Digit Recognition - Created method to test prediction

--- digitrec.py
+++ digitrec.py
@@ -6,6 +6,9 @@ import timeit
 
 # Import numpy as np
 import numpy as np
+
+
+import sys
 # Import tensorflow as tf
 #import tensorflow as tf
 #from tensorflow.examples.tutorials.mnist import input_data
@@ -18,11 +21,13 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
-
+# Import Cv2 for image processing
+import cv2
 # load dataset for global use
 (image_train, label_train), (image_test, label_test) = mnist.load_data()
 labelOptions = 10
 imageSize = 784
+
 
 def prepareDataSet():
     """ 
@@ -31,14 +36,11 @@ def prepareDataSet():
     global image_train
     global image_test
     global label_train
-    global label_test 
-    
+    global label_test
+
     print("Preparing Mnist...")
     #  (image_train, label_train), (image_test, label_test) = mnist.load_data()
-   
 
-   
-    
     # i,j act as offsets to offest idx format
     imageOffest = 16
     labelOffset = 8
@@ -56,17 +58,17 @@ def prepareDataSet():
     image_test /= 255
 
     # convert class vectors to binary class matrices
-    #A binary matrix representation of the input. The classes axis is placed last. 
+    # A binary matrix representation of the input. The classes axis is placed last.
     # Used for  categorical_crossentropy loss on model
     label_train = keras.utils.to_categorical(label_train, labelOptions)
     label_test = keras.utils.to_categorical(label_test, labelOptions)
-    print (label_test)
-    
+    print(label_test)
+
     # Output Statistics
     print(image_train.shape[0], 'training images loaded.')
     print(image_test.shape[0], 'test images loaded.')
     print("Preparing Mnist data Complete!")
-    
+
     # Start timer
     # start_time_train = timeit.default_timer()
 
@@ -89,15 +91,18 @@ def prepareDataSet():
 
 
 def buildNeuralNet():
-    
+
     global model
+    global score
     batch_size = 128
     #num_classes = 10
     epochs = 1
-    learning_rate = 1e-6
-    model = Sequential() # Using Sequental ,Linear stack of layers
-    model.add(Dense(512, activation='relu', input_shape=(784,)))# add layer off input shape 784 and output shape of *532
-    model.add(Dropout(0.2))# set fraction of input rates to drop during training
+    #learning_rate = 1e-6
+    model = Sequential()  # Using Sequental ,Linear stack of layers
+    # add layer off input shape 784 and output shape of *532
+    model.add(Dense(512, activation='relu', input_shape=(784,)))
+    # set fraction of input rates to drop during training
+    model.add(Dropout(0.2))
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.2))
     model.add(Dense(labelOptions, activation='softmax'))
@@ -105,7 +110,7 @@ def buildNeuralNet():
     # Print a string summary of the model
     model.summary()
 
-    # Configure model for training 
+    # Configure model for training
     # loss = name of objective function
     # metrics list of metrics to be evaluated by the model
     """
@@ -116,12 +121,12 @@ def buildNeuralNet():
     """
     # Compile the model
     # Using sgd gives .86 accur takinhg 18s
-    # Using RMSprop() gives .98 taking 23s 
+    # Using RMSprop() gives .98 taking 23s
     # rmsprop = RMSprop(lr=learning_rate) gives .28  taking 25s
     rmsprop = RMSprop()
     model.compile(loss='categorical_crossentropy',
-                optimizer=rmsprop,
-                metrics=['accuracy'])
+                  optimizer=rmsprop,
+                  metrics=['accuracy'])
 
     # Start timer and build model
     print("\nBuilding Model...")
@@ -134,7 +139,7 @@ def buildNeuralNet():
                         epochs=epochs,
                         verbose=1,
                         validation_data=(image_test, label_test))
-    
+
     # Stop Timer
     print("\nModel built !")
     end_time_train = timeit.default_timer() - start_time_train
@@ -142,10 +147,14 @@ def buildNeuralNet():
     score = model.evaluate(image_test, label_test, verbose=0)
     print('Test loss    :', score[0])
     print('Test accuracy:', score[1])
-    print("Time Taken to train model at ",epochs,"epochs =",end_time_train)
+    print("Time Taken to train model at ", epochs, "epochs =", end_time_train)
 
 
 def prediction():
+    print("pr")
+    
+
+def testPrediction():
     predictionB = model.predict(np.array([image_test[0]], dtype=float))
     print("Predicted B: ", predictionB)
     print("Actual: B", label_test[0])
@@ -153,25 +162,24 @@ def prediction():
     # prediction = model.predict(np.array([x_test[1]], dtype=float), as_iterable=False)
     #prediction = history.predict(np.array([image_test[1]], dtype=float))
     #bestPrediction= max()
-    #model.predict
+    # model.predict
 
     # Print our prediction and display the actual image we are trying to predict
     #print("Predicted A: ", prediction.index(max(prediction)))
     #print("Actual: A", label_test[1])
-    
 
 def userMenu():
     """
     Launches a menu for user input
     """
-    
+
     # print("tf-----",tf.__version__)
     # print("ker---",keras.__version__)
     netBuilt = False
     choice = True
 
     while choice:
-        print("\n==== MNIST DATASET DIGIT RECOGNITION ====\n1.Prepare and Setup image dataset\n2.Predict an image\n3.Exit")
+        print("\n==== MNIST DATASET DIGIT RECOGNITION ====\n1.Prepare and Setup image dataset\n2.Predict an image\n3.Test & confirm Prediction Model\n4.Exit")
         choice = input("Select Option? ")
         if choice == "1":
             if netBuilt:
@@ -185,12 +193,17 @@ def userMenu():
                 prediction()
             else:
                 print("Neural network not configured, select option 1 and try again !")
-            
         elif choice == "3":
+            if netBuilt:
+                testPrediction()
+            else:
+                print("Neural network not configured, select option 1 and try again !")
+        elif choice == "4":
             print("\n Exiting...")
             exit()
         elif choice != "":
-             print("\n Invalid Option Try again")
+            print("\n Invalid Option Try again")
+
 
 # Launch the main menu
 userMenu()
